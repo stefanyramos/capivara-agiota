ab1cb3cf90d16106b9d312fcffd4a6d057e45809
stefanyramos <40060993+stefanyramos@users.noreply.github.com>
2022-02-03 23:21:34 -0300

    update config

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 notion_token = "YOUR_NOTION_TOKE"
 telegram_token = "YOUR_TELEGRAM_TOKEN"
+chat_id = "TELEGRAM_CHAT_ID"
 
 membros = {
-    "name of the member in your notion": "his/her telegram @ "
+    "nome da pessoa no notion": "@ do telegram da pessoa ",
+    "Fulano da Silva": "@fulano", # exemplo
+    "": "" # não retire essa linha, se vc tirar ela, quando uma tarefa não tiver ninguem como responsavel o bot vai quebrar
 }
